785a8f76a885724a7b9c7c590d4fc23eb4ff85e7
hjdgvhjrj <hjdg@hjrj>
2026-04-09 17:41:00 +0530

    pattern 5

---
+++ Prefix_sufix_Sum/PivotIndex.java
@@ -0,0 +1,40 @@
+package Prefix_sufix_Sum;
+
+import java.util.*;
+
+public class PivotIndex {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        int result = pivotIndex(nums);
+
+        System.out.println("Pivot Index: " + result);
+    }
+
+    public static int pivotIndex(int[] nums) {
+
+        int total = 0;
+
+        // Step 1: calculate total sum
+        for (int num : nums) {
+            total += num;
+        }
+
+        int left = 0;
+
+        // Step 2: check each index
+        for (int i = 0; i < nums.length; i++) {
+
+            int right = total - left - nums[i];
+
+            if (left == right) {
+                return i;
+            }
+
+            left += nums[i];
+        }
+
+        return -1;
+    }
+}
---
+++ Prefix_sufix_Sum/prefixsum.java
@@ -0,0 +1,22 @@
+package Prefix_sufix_Sum;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+public class prefixsum {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        System.out.println(Arrays.toString(prefix(a)));
+        
+    }
+
+   static int[] prefix(int[] a){
+    int [] vec=new int[a.length];
+    vec[0]=a[0];
+   
+    for (int i = 1; i < a.length; i++) {
+        vec[i]=vec[i-1]+a[i];
+    }
+    return vec;
+   }
+}
